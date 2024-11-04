c9636cd26f0b5decbcc56a497a40bee8f04a0e44
azza-python <154397765+Azzimmka@users.noreply.github.com>
2024-11-03 18:33:40 -0800

    developed DB and admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
+from PIL.PyAccess import mode_map
 from django.contrib import admin
+from .models import Category, Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'views', 'created_at', 'updated_at')
+    list_filter = ('category', 'created_at')
+    readonly_fields = ('views',)
+
+admin.site.register(Category)
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    desc = models.TextField()
+    image = models.ImageField(upload_to='article/', null=True, blank=True)
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
+
